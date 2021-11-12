9eb5f04394a44ff88112be7084fd9f8ff10807a3
Luiz Vieira <luiz22_sp@hotmail.com>
2021-11-12 07:58:46 -0300

    Criando o primeiro componente

--- gerenciador-de-fotos/src/app/app.component.ts
+++ gerenciador-de-fotos/src/app/app.component.ts
@@ -6,6 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Leão';
-  url= 'https://i.ytimg.com/vi/W8GnWjW8Kaw/hqdefault.jpg';
+  photos = [
+    {
+      description: 'Leão',
+      url: 'https://i.ytimg.com/vi/W8GnWjW8Kaw/hqdefault.jpg'
+    },
+    {
+      description: 'Leoa',
+      url: 'https://s2.glbimg.com/awP2KnJnzIYEWAVOYLFL4d2nG0I=/0x0:2400x1599/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2018/a/m/0T0EZ4QZqcrtzp9ZwNeg/2018-10-21t152242z-534837741-rc174f509210-rtrmadp-3-usa-zoo-lions.jpg'
+    }
+  ]
+  
+  
 }
---
+++ gerenciador-de-fotos/src/app/photos/photos.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PhotoComponent } from './photo/photo.component';
+
+
+
+@NgModule({
+  declarations: [ PhotoComponent ],
+  imports: [
+    CommonModule
+  ],
+  exports: [ PhotoComponent ]
+})
+export class PhotosModule { }
